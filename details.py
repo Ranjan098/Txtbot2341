33da1502e3624ba43c7120730a3e341d3fcad12d
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2023-11-13 01:56:56 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 12606917
 api_hash = "f25113b8c17dca6fa7abda53a86bd4f7"
-bot_token = "6787006839:AAHPU9x0iC1R890VpjDuxuWbRZqHYhv3_Xc"
+bot_token = "6787006839:AAG0Q4lF87TcSEt4on-ZA9WQtVH03xVtQ40"
 sudo_groups = -1001699711548
